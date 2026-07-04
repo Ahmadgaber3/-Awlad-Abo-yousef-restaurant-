732c7b92694a2e9b338ec9a076910475f1e84388
Ahmadgaber3 <ans354093@gmail.com>
2026-07-04 20:41:33 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,8 +5,9 @@ const ASSETS = [
   './images/logooo.jpg'
 ];
 
-// تثبيت التطبيق في الذاكرة
+// السيرفيس وركر ده "بيسجل" الموقع كأنه تطبيق في ذاكرة الموبايل
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
@@ -14,11 +15,22 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// تشغيل التطبيق كأنه أبلكيشن
+// ده اللي بيخلي التطبيق يفتح بسرعة كأنه تطبيق فيسبوك
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+// ده بيخلي التطبيق "يحدّث" نفسه علطول
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cache) => cache !== CACHE_NAME).map((cache) => caches.delete(cache))
+      );
     })
   );
 });

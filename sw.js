29584166727654fb89a65525f6aff538607d9ad6
Ahmadgaber3 <ans354093@gmail.com>
2026-07-04 20:28:32 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,6 +5,7 @@ const ASSETS = [
   './images/logooo.jpg'
 ];
 
+// تثبيت التطبيق في الذاكرة
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -13,6 +14,7 @@ self.addEventListener('install', (event) => {
   );
 });
 
+// تشغيل التطبيق كأنه أبلكيشن
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
